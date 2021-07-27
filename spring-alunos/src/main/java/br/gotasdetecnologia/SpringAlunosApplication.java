96558f696646e3946ebae9da0b3f9a820b9cf9f8
evertonulisystem <evertonulisystem@gmail.com>
2021-07-26 21:02:00 -0300

    Criação do Projeto

---
+++ spring-alunos/src/main/java/br/gotasdetecnologia/SpringAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.gotasdetecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlunosApplication.class, args);
+	}
+
+}
---
+++ spring-alunos/src/test/java/br/gotasdetecnologia/SpringAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gotasdetecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
